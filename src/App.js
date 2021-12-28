d7cb17eb45d41389cd9852e873298ebb483e7a1c
Kelvin <kelvinkamau387@gmail.com>
2021-12-28 13:24:45 +0300

    Final Commit

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { uniq } from "lodash";
+import Tag from "./assets/kamfupi.png";
+
+import "./App.css";
+
+const App = () => {
+  const [codes, setCodes] = useState([]);
+  console.log(codes.length);
+  console.log(codes);
+
+  const addCodes = (e) => {
+    let codeArr = e.target.value.replace(/\s+/g, " ").split(",");
+
+    codeArr.forEach((code) => {
+      if (code.length > 1 && !codes.includes(code)) {
+        if (codes.length < 7) {
+          setCodes(uniq(codes.concat(codeArr).slice(0, 7)));
+          e.target.value = null;
+        }
+      }
+    });
+  };
+
+  const removeTags = (indexToRemove) => {
+    setCodes(codes.filter((_, index) => index !== indexToRemove));
+  };
+
+  const removeAll = () => setCodes([]);
+
+  return (
+    <div className="wrapper">
+      <div className="title">
+        <img src={Tag} alt="tag" />
+        <h2>URL short codes</h2>
+      </div>
+      <div className="content">
+        <p>Press enter or add a comma after each code</p>
+        <ul>
+          {codes.map((code, index) => (
+            <li key={index}>
+              {code}
+              <i
+                className="uit uit-multiply"
+                onClick={() => removeTags(index)}
+              ></i>
+            </li>
+          ))}
+
+          <input
+            type="text"
+            spellCheck="false"
+            onKeyUp={(e) => (e.key === "Enter" ? addCodes(e) : null)}
+          />
+        </ul>
+      </div>
+      <div className="details">
+        <p>
+          <span>{7 - codes.length}</span> short codes remaining
+        </p>
+        <button onClick={removeAll}>Clear All</button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
